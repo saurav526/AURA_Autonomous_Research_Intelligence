02045cf32d2fb544c0536fb7e5bedc1e14864db6
saurav526 <sauravtiwari2712005@gmail.com>
2026-09-13 01:09:36 +0530

    initalization config

---
+++ backend/app/__init__.py
@@ -0,0 +1,2 @@
+# This file is blank because the backend is a Python package and does not require any initialization code.
+#  this is important because it allows the backend to be treated as a package and enables the use of relative imports within the package.
